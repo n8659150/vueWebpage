145a6dbfed2d49dd5998fa962e3808c7ec444378
n8659150 <jackli900421@hotmail.com>
2017-09-17 21:39:44 +0800

    created necessary files using vue-cli webpack-simple template.

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <news></news>
+    {{msg}}
+  </div>
+</template>
+
+<script>
+import News from './News.vue'
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components:{
+    news:News
+  }
+}
+</script>
+<style>
+</style>
